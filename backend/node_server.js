7807b6f1e4e27c832a20ae4bb6f0308a370c2fa3
Velmurugan <velmurugan.web@gmail.com>
2018-03-11 05:02:07 +0530

    Importing LocalDate fixed

--- backend/node_server.js
+++ backend/node_server.js
@@ -98,7 +98,7 @@ app.get('/employee/:eId', function(req, res){
 
 // Handling data from POST from client side
 app.put('/employee/:eId/:wId/:fId/:vId', function(req, res){
-    console.log("/employee/eId of employee : "+req.params.eId+" "+req.params.Id+" "+req.params.vId);
+    console.log("/employee/eId of employee : "+req.params.eId+" "+req.params.fId+" "+req.params.vId);
     dbService.dbUpdateResourceByEmpId(req.params.eId,req.params.wId,req.params.fId,req.params.vId,function(data){
             res.send(data);
         });
--- backend/services/dbService.js
+++ backend/services/dbService.js
@@ -1,8 +1,9 @@
-var ArrayList = require('arraylist');
-var cassandra = require('cassandra-driver');
-var client = new cassandra.Client({contactPoints: ['127.0.0.1'], keyspace: 'wipapp'});
+const ArrayList = require('arraylist');
+const cassandra = require('cassandra-driver');
+const LocalDate = require('cassandra-driver').types.LocalDate;
+const client = new cassandra.Client({contactPoints: ['127.0.0.1'], keyspace: 'wipapp'});
 
-var nonDateFields = new ArrayList;
+const nonDateFields = new ArrayList;
 nonDateFields.add(['s1l1cm','s1l2cm','s2l2cm']);
 
 module.exports = {
@@ -47,18 +48,13 @@ module.exports = {
 		// Logic to check fields to be updated is date or string
 		// if date then parse in to LocalDate format, which cassandra supports
 		if(!nonDateFields.contains(fieldId)){
-			valId = new LocalDate.fromString(valId); 
+			valId = LocalDate.fromString(valId); 
 		}
-		const param = [fieldId,valId,empId,wbsId];
-		client.execute(query,param,{ prepare: true }, function (err, result) {
+		const param = [valId,empId,wbsId];
+		client.execute(query,param, { prepare: true },function (err, result) {
 			if (!err){
-	           if ( result.rows.length > 0 ) {
-	           		console.log(result.rows);
-	           		return callback(result.rows);
-	            } else {
-	            	console.log("No results");
-	           }
-	       }else{
+				return callback("SUCCESS");
+	        }else{
 	       		console.log(err);
 	       }
     	});
